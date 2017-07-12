5585244624c5e0c6079c203bfc556d9888cb5e00
Hoang Anh Vo <vohoanganhkhtn@gmail.com>
2017-07-12 22:04:54 +0300

    add module axios

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class HomePage extends React.Component{
+  handleRequest(){
+    // axios.get('/try')
+    // .then(res => console.log(res.data))
+    // .catch(err => console.log(err))
+
+    axios.post('/axios', {username: 'hoanganh'})
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+  }
   render(){
     return (
       <div>
         <h1>This is Homepage</h1>
+        <button onClick={this.handleRequest.bind(this)}>request</button>
       </div>
     )
   }
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
+var jsonParser = require('body-parser').json();
+
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server started'))
 app.get('/', (req, res) => res.render('home'));
+
+app.get('/try', (req, res)=>{
+  res.send('Hello Axios');
+})
+
+app.post('/axios', jsonParser, (req, res)=>{
+  var text = req.body.username +' dep trai'
+  res.send(text);
+})
